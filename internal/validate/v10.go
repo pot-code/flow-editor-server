e8358f84514ab379ee7f77b5508407f555f2dc25
pot-code <136290354@qq.com>
2024-06-08 00:44:16 +0800

    refactor: init validation translator

--- internal/validate/v10.go
+++ internal/validate/v10.go
@@ -10,23 +10,29 @@ import (
 	zh_translation "github.com/go-playground/validator/v10/translations/zh"
 )
 
-func NewValidator(trans ut.Translator) *validator.Validate {
+func NewValidator() *validator.Validate {
 	v := validator.New(validator.WithRequiredStructEnabled())
 
-	zh_translation.RegisterDefaultTranslations(v, trans)
 	v.RegisterTagNameFunc(func(fld reflect.StructField) string {
-		name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
+		tag := fld.Tag.Get("json")
+		if tag == "" {
+			return fld.Name
+		}
+
+		name := strings.SplitN(tag, ",", 2)[0]
 		if name == "-" {
-			return ""
+			return fld.Name
 		}
 		return name
 	})
 	return v
 }
 
-func NewTranslator() ut.Translator {
+func NewTranslator(v *validator.Validate) ut.Translator {
 	zh := zh.New()
 	uni := ut.New(zh, zh)
 	trans, _ := uni.GetTranslator("zh")
+
+	zh_translation.RegisterDefaultTranslations(v, trans)
 	return trans
 }
